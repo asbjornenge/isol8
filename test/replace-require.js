aba5075457669c7148b27c0ffb3d8df2321c10cc
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-25 11:52:36 +0100

    replace-require as a module

--- test/replace-require.js
+++ test/replace-require.js
@@ -1,19 +1,19 @@
 import assert from 'assert'
 import fs     from 'fs'
 import path   from 'path'
-import isol8  from '../index'
+import rr     from '../replace-require'
 
 let src = fs.readFileSync(path.resolve(__dirname, './app/app.js'),'utf-8')
 
 it('replaces the entire require statement', () => {
-    let __src = isol8(src, (target) => {
+    let __src = rr(src, (target) => {
         if (target.indexOf('.png') >= 0) return "require('yolo')"
     })
     assert(__src.indexOf("var image = require('yolo')") >= 0)
 })
 
 it('can replace a require statement with anything', () => {
-    let __src = isol8(src, (target) => {
+    let __src = rr(src, (target) => {
         if (target.indexOf('.png') >= 0)  return "\"base64\""
         if (target.indexOf('.styl') >= 0) return "\"body { color : red; }\""
     })
